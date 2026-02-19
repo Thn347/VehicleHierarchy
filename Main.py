3d745b52ba28fda60f868b0d5ed60d6385a7705d
Thn347 <thn5129@psu.edu>
2026-02-18 21:37:25 -0500

    New auto_model class
    + defensive copy for years attribute

--- Main.py
+++ Main.py
@@ -5,17 +5,28 @@ Author: Thuan Nguyen
 
 # from <file> import <class>
 from manufacturer import Manufacturer
+from auto_model import AutoModel
 
 def main():
     # print("Hello World")
     # Manufacturer.print_me()
-    m = Manufacturer("Ford", "USA")
+    # m = Manufacturer("Ford", "USA")
     # m.print_me()
     # print(m._name)   #DO NOT access private variable
-    print(m.get_name) 
-    print(m.get_country)
-    print(m)
-    pass
+    # print(m.get_name) 
+    # print(m.get_country)
+    # print(m)
+    original_list = [2020, 2021]
+    am = AutoModel("F150", True, original_list)
+
+    print(am.get_years)
+
+    original_list.clear()
+
+    original_list.append(2022)
+
+    print(am.get_years)
+    # pass
 
 if __name__ == "__main__":
     main()
