92dd1fe64538b7108be7457d04b2885412e240af
Thn347 <thn5129@psu.edu>
2026-02-25 15:20:49 -0500

    The vehicle.py

---
+++ vehicle.py
@@ -0,0 +1,31 @@
+from abc import ABC, abstractmethod
+# from functools import total_ordering  # sorting purposes
+
+from manufacturer import Manufacturer
+from auto_model import AutoModel
+
+class Vehicle(ABC):
+    """
+    Abstract base class (ABC) for all vehicles
+    """
+
+    # constructor
+    def __init__(self,
+                 manufacturer: Manufacturer,
+                 model: AutoModel,
+                 mpg: float):
+        self._manufacturer = manufacturer
+        self._model = model
+        self._mpg = mpg
+
+    @property
+    def manufacturer(self) -> Manufacturer:
+        return self._manufacturer
+    
+    @property
+    def automodel(self) -> AutoModel:
+        return self._model
+    
+    @property
+    def mpg(self) -> float:
+        return self._mpg
